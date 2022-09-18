a43302e1a1f89a1b57623f3aa02358e12c3edde6
bagasadinugroho28 <bagasadinugroho2810@gmail.com>
2022-09-18 12:19:28 +0700

    contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Bagas Adi Nugroho,
+            Saya Tinggal di Daerah Cakung,
+            Olahraga Yang Saya Sukai Adalah
+            Bulu tangkis dan Lari.";
+    }
+}
